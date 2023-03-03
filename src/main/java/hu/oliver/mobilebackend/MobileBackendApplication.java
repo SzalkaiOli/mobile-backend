b8be831146a14713c9ff5e53a7d86e2f02197a27
SzalkaiOli <onlq05@inf.elte.hu>
2023-03-03 21:04:35 +0100

    Creating base of the application
    Creating the base of the application with a working server but instead of MySQL, making it with PostgreSQL

---
+++ src/main/java/hu/oliver/mobilebackend/MobileBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.oliver.mobilebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/oliver/mobilebackend/MobileBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.oliver.mobilebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
